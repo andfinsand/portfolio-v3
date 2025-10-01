e90d49a4bac98486691d2ac06248c591ce983951
Andrew Finsand <andfinsand@gmail.com>
2025-09-30 21:37:42 -0700

    Add more bottom padding to main container

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -6,7 +6,7 @@ import About from "./About";
 
 export default function Center() {
     return (
-        <div className="flex flex-col items-center w-full h-fit gap-16 sm:gap-24 lg:gap-32 px-5 lg:px-14 py-2 sm:py-20 lg:py-36 xl:py-40">
+        <div className="flex flex-col items-center w-full h-fit gap-16 sm:gap-24 lg:gap-32 px-5 lg:px-14 pt-2 pb-16 sm:py-20 lg:py-36 xl:py-40">
             < Banner />
             < SkillsCarousel isMobile={false} />
             {/* Projects Section */}
